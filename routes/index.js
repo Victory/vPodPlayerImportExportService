9027a1844e6b8d104cfa087ef72bf76acaee8dae
Victory <git@dfhu.org>
2017-03-20 19:50:30 -0500

    write json to disk

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs')
+const path = require('path');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Import Export for vPodPlayer' });
+});
+
+function writeJs(res, fn, data) {
+    fs.writeFile(fn, data, function (err) {
+        if (err) {
+            res.send(JSON.stringify({success: false, msg: err , data: {}}));
+            return;
+        }
+        res.send(JSON.stringify({success: true, msg: "saved", data: {key: exportKey}}));
+    })
+};
+
+/* POST a dump */
+router.post('/', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+
+    for (var ii = 1; ii < 1000; ii++)
+    {
+        var exportKey = "exports" + ("" + (1000 + ii)).substr(1, 4);
+        var uploadDir = req.app.get('uploadDir');
+        var exportFilename = path.join(uploadDir, exportKey + ".json");
+        if (!fs.existsSync(exportFilename)) {
+            fs.openSync(exportFilename, 'w');
+            var json = JSON.stringify(req.body);
+            writeJs(res, exportFilename, json);
+            break;
+        }
+    }
+
 });
 
 module.exports = router;
---
+++ uploades/README.md
@@ -0,0 +1,1 @@
+This directory will contain the uploaded export json files
